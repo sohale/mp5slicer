85cb3fbff05157c5f6a581f04bbdc68ac9044694
Flavien Rousseaux <flavien.rousseaux@isen-lille.fr>
2016-05-10 10:48:29 +0100

    fix: an island inside an island is not printed

--- Island_stack.py
+++ Island_stack.py
@@ -11,8 +11,10 @@ class Island_stack():
         for child in polynode.Childs:
             if not child.IsHole:
                 self.islands.append(child)
-                if child.depth > 1:
+                if child.depth > 0:
                     self.split(child)
+            elif child.depth > 0:
+                self.split(child)
 
     def get_islands(self):
         return self.islands
