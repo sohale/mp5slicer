266618be7a874a6fe77464cf528f3b387975c256
Charles <charles.cartiaux@isen-lille.fr>
2016-06-02 12:23:49 +0100

    re-add IslandMapEdge

---
+++ IslandMapEdge.py
@@ -0,0 +1,9 @@
+
+class IslandMapEdge:
+    def __init__(self,source,dest,cost,path):
+        """ initializes a graph object """
+        self.source = source
+        self.dest = dest
+        self.cost = cost
+        self.path = path
+
