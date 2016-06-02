68d08c45bbd3bcb07f9a79130dea36bdf67eef0c
Flavien Rousseaux <flavien.rousseaux@isen-lille.fr>
2016-06-02 12:32:07 +0100

    added test campaign

---
+++ testcampaign.py
@@ -0,0 +1,9 @@
+import os
+
+os.system("python Print_pipeline.py stl/elephant.stl config.json")
+os.system("python Print_pipeline.py stl/XY_resonance_test.stl config.json")
+# os.system("python Print_pipeline.py stl/tealight_dbl_layer.stl config.json")
+os.system("python Print_pipeline.py stl/negative_space_tolerance_test.stl config.json")
+os.system("python Print_pipeline.py stl/fine_positive_features.stl config.json")
+# os.system("python Print_pipeline.py stl/XY_resonance_test.stl config.json")
+# os.system("python Print_pipeline.py stl/XY_resonance_test.stl config.json")
