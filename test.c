a72ac681186bc63876cd9c4f29052be1a601e1ab
itheof <tvallee@student.42.fr>
2016-07-08 18:01:16 +0100

    slicer worker improvements

--- test.c
+++ test.c
@@ -5,32 +5,97 @@
 
 /* This is a slicer-wrapper compliant program. */
 
+typedef struct      s_list {
+
+    char            *data;
+    struct s_list   *next;
+}                   t_list;
+
+t_list *listnew
+(char *content)
+{
+    t_list *new;
+
+    if (new = malloc(sizeof(*new))) {
+        new->data = strdup(content);
+        new->next = NULL;
+        return new;
+    }
+    else {
+        return NULL;
+    }
+}
+
+t_list *list_pushback
+(t_list **list, t_list *new)
+{
+    t_list *current;
+
+    if (*list) {
+        current = *list;
+        while (current->next) {
+            current = current->next;
+        }
+        current->next = new;
+    }
+    else
+        *list = new;
+    return *list;
+}
+
+size_t list_len
+(t_list *list)
+{
+    size_t len;
+
+    len = 0;
+    while (list) {
+        list = list->next;
+        len++;
+    }
+    return len;
+}
+
 int main
 (int ac, char **av)
 {
     int     i;
     char    *buf = NULL;
     size_t  size;
+    size_t  len;
+    t_list  *list;
+    t_list  *old;
 
     for (i=0;i<ac;i++) {
-        fputs(av[i], stderr);
-        fputc(10, stderr);
-        sleep(1);
         if (!strcmp(av[i], "--crash"))
             exit(1);
         if (!strcmp(av[i], "--segv"))
             *buf = 0;
     }
 
-    i = 0;
+    list = NULL;
     while (getline(&buf, &size, stdin) != -1) {
-        if (i) {
-            fputs(buf, stdout);
-            fputc(10, stdout);
-            i = 0;
+        list_pushback(&list, listnew(buf));
+    }
+
+    len = list_len(list);
+    i = 0;
+    while (i < 100 && list)
+    {
+        if (i % 2) {
+            fprintf(stdout, list->data);
+//            fputc(10, stdout);
         }
-        else
-            i = 1;
+        else {
+            fprintf(stderr, "err: %s\n", list->data);
+        }
+        i++;
+        old = list;
+        list = list->next;
+        free(old->data);
+        free(old);
+        fprintf(stderr, ">>>>%d\n", i * 100 / len);
+        sleep(1);
     }
     return(0);
 }
--- wrapper.py
+++ wrapper.py
@@ -41,29 +41,11 @@ import requests
 
 
 def usage():
-    print('./wrapper.py')
+    print('usage: ./wrapper.py')
+    exit()
 
 def parse_args(settings):
     return ['--{}={} '.format(key, settings[key]) for key in settings]
-
-class Storage():
-
-    @staticmethod
-    def __connect():
-        couchdb.Server(url='http://couchdb:5984')
-
-    @staticmethod
-    def get_object(storage_ID):
-        return Storage.__connect()['objects'][storage_ID]['ObjectMP5']
-
-    @staticmethod
-    def put_slice(task, sliced):
-        return Storage.__connect()['objects'].put_attachment(
-            server['objects'][task.storage_ID],
-            sliced,
-            filename=('sliced.gcode')
-        )
-
 
 class Redis():
 
@@ -73,16 +55,11 @@ class Redis():
             port= 6379,
             db= 1
         )
-        return self
-
-    def __del__(self):
-        self.client.exit()
-        del self.client
 
     def pop(self):
         while True:
             try:
-                return self.client.blpop('s', 0)
+                return int(self.client.blpop('s', 0)[1])
             except:
                 print(sys.argv[0], ": blpop() failed.")
                 pass
@@ -99,7 +76,7 @@ def execute(task, broadcast):
     (r_stdout, w_stdout) = os.pipe()
     (r_stderr, w_stderr) = os.pipe()
     (r_stdin, w_stdin)   = os.pipe()
-    err = []
+    err = ''
 
 
     args = [WRAPPER_SETTINGS['exec']]
@@ -110,12 +87,12 @@ def execute(task, broadcast):
             os.close(fd)
         errs = os.fdopen(r_stderr)
         ok  = os.fdopen(r_stdout)
-        os.write(w_stdin, bytes(string(task['obj']['root']), 'utf-8'))
+        os.write(w_stdin, bytes(str(task['obj']['root']), 'utf-8'))
         os.close(w_stdin)
         for line in errs:
             if line.startswith(WRAPPER_SETTINGS['progress_mark']):
-                broadcast(line[len(WRAPPER_SETTINGS['progress_mark']):])
-            err.append(line)
+                broadcast(line[len(WRAPPER_SETTINGS['progress_mark']):-1])
+            err += line
         errs.close()
         pid, status = os.waitpid(father, 0)
         if status is not 0:
@@ -124,13 +101,13 @@ def execute(task, broadcast):
         else:
             sliced = ok.read()
             ok.close()
-            return sliced
+            return sliced, err
     else:
         for fd in [w_stdin, r_stderr, r_stdout]:
             os.close(fd)
         for (old, new) in [(1, w_stdout), (2, w_stderr), (0, r_stdin)]:
             os.dup2(new, old)
-        os.execv('./a.out', args)
+        os.execv('./a.out', ['./a.out',])#args)
         os.exit(0)
 
 
@@ -145,7 +122,7 @@ def get_settings():
     }
     opts = True
 
-    for arg in sys.argv:
+    for arg in sys.argv[1:]:
 
         if opts and arg is '--':
             opts = False
@@ -179,22 +156,47 @@ def main():
         should_quit = True
     signal.signal(signal.SIGINT, gracefully_exit)
 
+    #Next section up to the while is to be deleted
+    f = open('./testfile')
+    task = {
+        "obj": {
+            "root": f.read()
+        },
+        "settings": {}
+    }
+
     while not should_quit:
 
         r = Redis()
+        running = False
         pk = r.pop() # blocking until a task is actually retrieved
-        url = WRAPPER_SETTINGS['django_host'] + pk
-        task = requests.get(url + '/start/', data = {}) # /!\ FIX ME: Need to authenticate
+        if should_quit:
+            print("This is a complicated edge case to handle. We should probably abort and report to the API if we still have the time")
+            exit(1)
+        url = WRAPPER_SETTINGS['django_host'] + str(pk)
+#TODO:API request        task = requests.get(url + '/start/', data = {}) # /!\ FIX ME: Need to authenticate
         traceback = None
         try:
-            sliced = execute(task, lambda x: r.broadcast_progress(pk, x))
+            sliced, debug = execute(task, lambda x: r.broadcast_progress(pk, x))
+            r.broadcast_progress(pk, 100)
         except Exception as inst:
+            print(inst)
             print('ERROR: process exited with status code {}, printing full error log:'.format(inst.args[0]))
             print(inst.args[1])
             traceback = inst.args[1]
-        ret = requests.patch(url + '/end/', data = {'status':0, 'logs':''})
-        r.broadcast_progress(pk, 100)
+            r.broadcast_progress(pk, -1)
+#TODO:API request        ret = requests.patch(url + '/end/', data = {'status':0, 'logs':''})
 
+        # Remove the following when implemented correctly
+        if traceback is not None:
+            print("++++++++++++++ ERROR ENCOUNTERED +++++++++++++++++++")
+            print(traceback)
+        else:
+            print("============== FINAL OUTPUT ========================")
+            print(sliced)
+            print()
+            print("==============  DEBUG LOGS  ========================")
+            print(debug)
 
 if __name__ == '__main__':
     main()
