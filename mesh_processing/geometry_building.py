1fc38e49bb0aae1590559409c4135ea8d3afa4f6
flavien <flv.rousseaux@gmail.com>
2016-10-18 18:29:50 +0100

    simplified slicer entrypoint

---
+++ mesh_processing/geometry_building.py
@@ -0,0 +1,53 @@
+from slicer.shapes.mp5totree import get_mc_params
+import json
+import numpy as np
+import pymplicit
+from stl import mesh
+
+
+def build_geometry(mp5):
+    stls = []
+    for son_position in range(len(mp5["root"]["children"])):
+        mc = get_mc_params(mp5, son_position)
+        mc_params = to_json_mc_params(mc)
+        mp5_string = json.dumps(mp5["root"]["children"][son_position])
+
+
+        pymplicit.build_geometry(mp5_string, mc_params)
+        verts = pymplicit.get_verts()
+        faces = pymplicit.get_faces()
+        pymplicit.finish_geometry()
+
+        stl = m2stl_mesh(verts, faces)
+        stls.append(stl.data)
+
+        return mesh.Mesh(np.concatenate(stls))
+
+
+def m2stl_mesh(verts, faces):
+
+    fv = verts[faces, :]
+
+    data = np.zeros(fv.shape[0], dtype=mesh.Mesh.dtype)
+    for i in range(fv.shape[0]):
+        facet = fv[i]
+        data['vectors'][i] = facet
+
+    m = mesh.Mesh(data)
+    return m
+
+
+def to_json_mc_params(bbox):
+    bb = {}
+    bb["xmin"] = bbox.min.x.item(0)
+    bb["xmax"] = bbox.max.x.item(0)
+
+    bb["ymin"] = bbox.min.y.item(0)
+    bb["ymax"] = bbox.max.y.item(0)
+
+    bb["zmin"] = bbox.min.z.item(0)
+    bb["zmax"] = bbox.max.z.item(0)
+    ignore_root_matrix = False
+    mc_params = {"resolution": 200, "box": bb, "ignore_root_matrix": ignore_root_matrix}
+    mc_params = json.dumps(mc_params)
+    return mc_params
--- print_from_file_cpp.py
+++ print_from_file_cpp.py
@@ -6,11 +6,9 @@ sys.path.append(os.path.split(os.path.dirname(os.path.abspath(inspect.getfile(in
 
 from slicer.Print_pipeline import print_mesh
 from slicer.config.config_factory import ConfigFactory
-from slicer.shapes.mp5totree import get_mc_params
 import json
-import numpy as np
-import pymplicit
-from stl import mesh
+from slicer.mesh_processing.geometry_building import build_geometry
+
 
 
 def print_from_mp5():
@@ -29,60 +27,18 @@ def print_from_mp5():
 
     if 'printerSettings' in mp5:
         ConfigFactory(dict_conf=dict_conf)
-        raise NameError(dict_conf_file)
     else:
         ConfigFactory()
     import slicer.config.config as config
     config.reset()
 
-    stls = []
-    for son_position in range(len(mp5["root"]["children"])):
-        mc = get_mc_params(mp5, son_position)
-        mc_params = to_json_mc_params(mc)
-        mp5_string = json.dumps(mp5["root"]["children"][son_position])
 
 
-        pymplicit.build_geometry(mp5_string, mc_params)
-        verts = pymplicit.get_verts()
-        faces = pymplicit.get_faces()
-        pymplicit.finish_geometry()
-
-        stl = m2stl_mesh(verts, faces)
-        stls.append(stl.data)
-
-    combined_stl = mesh.Mesh(np.concatenate(stls))
-
-    combined_stl.save("mp5.stl")
-    print_mesh(combined_stl, "mp5")
+    combined_mesh = build_geometry(mp5)
+    print_mesh(combined_mesh, "mp5")
 
 
-def m2stl_mesh(verts, faces):
 
-    fv = verts[faces, :]
-
-    data = np.zeros(fv.shape[0], dtype=mesh.Mesh.dtype)
-    for i in range(fv.shape[0]):
-        facet = fv[i]
-        data['vectors'][i] = facet
-
-    m = mesh.Mesh(data)
-    return m
-
-
-def to_json_mc_params(bbox):
-    bb = {}
-    bb["xmin"] = bbox.min.x.item(0)
-    bb["xmax"] = bbox.max.x.item(0)
-
-    bb["ymin"] = bbox.min.y.item(0)
-    bb["ymax"] = bbox.max.y.item(0)
-
-    bb["zmin"] = bbox.min.z.item(0)
-    bb["zmax"] = bbox.max.z.item(0)
-    ignore_root_matrix = False
-    mc_params = {"resolution": 200, "box": bb, "ignore_root_matrix": ignore_root_matrix}
-    mc_params = json.dumps(mc_params)
-    return mc_params
 
 if __name__ == '__main__':
     print_from_mp5()
