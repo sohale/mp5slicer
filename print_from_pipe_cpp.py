5faee89890cb2d1c5ba309bcb3c35877ebe598f2
flavien <flv.rousseaux@gmail.com>
2016-10-18 18:37:53 +0100

    simplified slicer entrypoint 2

--- print_from_pipe_cpp.py
+++ print_from_pipe_cpp.py
@@ -11,6 +11,7 @@ import json
 import numpy as np
 import pymplicit
 from stl import mesh
+from slicer.mesh_processing.geometry_building import build_geometry
 
 
 def print_from_mp5():
@@ -35,52 +36,9 @@ def print_from_mp5():
     import slicer.config.config as config
     config.reset()
 
-    stls = []
+    combined_mesh = build_geometry(mp5)
+    print_mesh(combined_mesh, "mp5")
 
-    for son_position in range(len(mp5["root"]["children"])):
-        mc = get_mc_params(mp5, son_position)
-        mc_params = to_json_mc_params(mc)
-        mp5_string = json.dumps(mp5["root"]["children"][son_position])
-
-        pymplicit.build_geometry(mp5_string, mc_params)
-        verts = pymplicit.get_verts()
-        faces = pymplicit.get_faces()
-        pymplicit.finish_geometry()
-
-        stl = m2stl_mesh(verts, faces)
-        stls.append(stl.data)
-
-    stl = mesh.Mesh(np.concatenate(stls))
-
-    # stl = puppy_magic(mp5)
-    # stl.save("mp5.stl")
-    print_mesh(stl, "mp5")
-
-
-def m2stl_mesh(verts, faces):
-
-    fv = verts[faces, :]
-
-    data = np.zeros(fv.shape[0], dtype=mesh.Mesh.dtype)
-    for i in range(fv.shape[0]):
-        facet = fv[i]
-        data['vectors'][i] = facet
-
-    m = mesh.Mesh(data)
-    return m
-
-
-def to_json_mc_params(bbox):
-    bb = {
-        'xmin': bbox.min.x.item(0), 'xmax': bbox.max.x.item(0),
-        'ymin': bbox.min.y.item(0), 'ymax': bbox.max.y.item(0),
-        'zmin': bbox.min.z.item(0), 'zmax': bbox.max.z.item(0)
-    }
-
-    ignore_root_matrix = False
-    mc_params = {"resolution": 40, "box": bb, "ignore_root_matrix": ignore_root_matrix}
-    mc_params = json.dumps(mc_params)
-    return mc_params
 
 
 if __name__ == '__main__':
